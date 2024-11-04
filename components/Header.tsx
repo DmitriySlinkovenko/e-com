01dc8dfe58c8cc59f9f727c8e56fce80a2071175
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-11-04 13:36:21 -0500

    Finished orderType & productType schema

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import React from 'react';
-import { ClerkLoaded, SignInButton, UserButton, useUser } from '@clerk/nextjs';
+import {
+  ClerkLoaded,
+  SignedIn,
+  SignInButton,
+  UserButton,
+  useUser
+} from '@clerk/nextjs';
 import Link from 'next/link';
 import Form from 'next/form';
 import { PackageIcon, TrolleyIcon } from '@sanity/icons';
@@ -9,9 +15,17 @@ import { PackageIcon, TrolleyIcon } from '@sanity/icons';
 const Header = () => {
   const { user } = useUser();
 
+  const createClerkPasskey = async () => {
+    try {
+      const res = await user?.createPasskey();
+    } catch (err) {
+      console.error('Error:', JSON.stringify(err, null, 2));
+    }
+  };
+
   return (
     <header className="flex flex-wrap justify-between items-center px-4 py-2">
-      <div>
+      <div className="flex w-full flex-wrap justify-between items-center">
         <Link
           href="/"
           className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
@@ -30,7 +44,7 @@ const Header = () => {
           />
         </Form>
 
-        <div>
+        <div className="items-center flex space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
           <Link
             href="/basket"
             className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -40,7 +54,7 @@ const Header = () => {
           </Link>
 
           <ClerkLoaded>
-            {user && (
+            <SignedIn>
               <Link
                 href="/orders"
                 className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -48,7 +62,7 @@ const Header = () => {
                 <PackageIcon className="w-6 h-6" />
                 <span>My Orders</span>
               </Link>
-            )}
+            </SignedIn>
 
             {user ? (
               <div className="flex items-center space-x-2">
@@ -60,6 +74,15 @@ const Header = () => {
               </div>
             ) : (
               <SignInButton mode="modal" />
+            )}
+
+            {user?.passkeys.length === 0 && (
+              <button
+                onClick={createClerkPasskey}
+                className="bg-white hover:bg-blue-700 hover:text-white animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
+              >
+                Create passkey
+              </button>
             )}
           </ClerkLoaded>
         </div>
