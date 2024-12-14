1b5e91408413b863ec40a64cfca6ed7362813523
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-12-13 20:47:44 -0500

    Searching by product name implemented, sale banner implemented

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,3 +1,4 @@
+import BlackFridayBanner from '@/components/BlackFridayBanner';
 import ProductsView from '@/components/ProductsView';
 import { Button } from '@/components/ui/button';
 import { getAllCategories } from '@/sanity/lib/products/getAllCategories';
@@ -12,9 +13,10 @@ export default async function Home() {
   //   crypto.randomUUID().slice(0, 5) +
   //     `>>> Rendered the home page cache with ${products.length} products and ${categories.length} categories`
   // );
+
   return (
     <div>
-      <h1>Hello World 123</h1>
+      <BlackFridayBanner />
 
       <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
         <ProductsView products={products} categories={categories} />
